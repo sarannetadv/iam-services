f1a0c7712801981f1429e439821bbd07b0de748c
sarannetadv <saran.netadv@gmail.com>
2025-10-25 00:38:14 +0530

    initil commit

---
+++ iam-services/src/test/java/com/mywork/iam/security/iam_services/IamServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywork.iam.security.iam_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
